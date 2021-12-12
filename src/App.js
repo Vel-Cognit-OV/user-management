402c84befd0d2baf337b94e731fb5ff03ba93394
FIRST_NAME LAST_NAME <kumaravelu.k@openvision.ai>
2021-12-12 18:18:28 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,81 +1,81 @@
-import React,{useState,useEffect} from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function AddForm(props){
-  const[pName, setPName] = useState(props.playerName);
-  const[tName, setTName] = useState(props.teamName);
+function PlayerData(props){
 
+  const [pName, setPName] = useState("");
+  const [tName, setTName] = useState("");
+  const [dataRow, setDataRow] = useState([]);
 
-  const[dataRow,settdataRow] = useState([]);
-
-  const[msg,setMsg]= useState("");
+  const [msg, setMsg] = useState("");
 
   useEffect(()=>{
-    setMsg("Alert Msgg");
+    setMsg("Successfully Added!");
     setTimeout(()=>setMsg(""),3000)
-  },[dataRow]);
+  },[dataRow])
 
-  function onChangeName(a){
-    setPName(a.target.value);
+  function pNameVal(e){
+    setPName(e.target.value)
   }
-  function onChangeTeam(a){
-    setTName(a.target.value);
+  function tNameVal(e){
+    setTName(e.target.value)
   }
-  function createPlayer(){
-    let pd = {"PlayerName":pName, "TeamName":tName};
-   let newRows=[...dataRow,pd];
-   settdataRow(newRows);
-
+  function createUser(){
+    if(pName.length > 0){
+      let pd = {"playerName":pName, "teamName":tName};
+      let newRow = [...dataRow,pd];
+      setDataRow(newRow);
+      setPName("");
+      setTName("");
+    }
   }
-
+  
+  function removeRow(a){
+    let newArr = [...dataRow];
+    newArr.splice(a,1);
+    setDataRow(newArr);
+  }
   return(
-    <>
-      <div>
-        <span style={{paddingRight:"30px"}}>
-          <label style={{paddingRight:"10px"}}>Player Name</label><input value={pName} onChange={onChangeName}/>          
-        </span>
-        <span>          
-          <label style={{paddingRight:"10px"}}>Team Name</label><input value={tName} onChange={onChangeTeam}/>
-        </span>
-        <span style={{paddingLeft:"30px"}}>
-          <input type="submit" value="+ Create" onClick={createPlayer}/>
-        </span>
-      </div>
-      <span style={{}}>{msg}</span>
-      <table border={"1"} width={"100%"} style={{marginTop:"50px"}}>
-        <tr>
-          <th>S No</th>
+    <div>
+        <div style={{padding:"25px"}}>
+           <label for="pName" style={{paddingRight:"30px"}}>
+             <span style={{paddingRight:"10px"}}>Player Name</span>
+             <input type="text" value={pName} id="pName" onChange={pNameVal}/>
+           </label>
+           <label for="tName" style={{paddingRight:"30px"}} id="pName">
+             <span style={{paddingRight:"10px"}}>Team Name</span>
+             <input type="text" value={tName} id="tName" onChange={tNameVal}/>
+           </label>
+           <input type="submit" value="+ Add" onClick={createUser}/>
+        </div>
+        <div style={{textAlign:"center", background:"#c3c3c3"}}>{msg}</div>
+        <table border="1" cellPadding="5" width="100%" style={{margin:"20px"}}>
+        <thead>
           <th>Player Name</th>
           <th>Team Name</th>
-          <th>Action</th>
-        </tr>
-        {dataRow.map((rowItem)=>(
-          <tr>
-          <td>1</td>
-          <td>{rowItem.PlayerName}</td>
-          <td>{rowItem.TeamName}</td> 
-          <td></td>
-        </tr>
-        ))}
-      </table>
-
-    </>
-
-  );
-}
-
-
-function App() {
-  function addPlayer(data){
-    console.log(data);
-  }
-  return (
-    <div className="App">      
-      <h1>Team Management</h1>
-      <AddForm playerName={""} teamName={""} getData={addPlayer}/>
-      {/* <UserList userDetail={""}/> */}
+          <th>Skill</th>
+          <th>Option</th>
+        </thead>
+        <tbody>
+          {dataRow.map((item,a)=>(
+            <tr>
+            <td>{item.playerName}</td>
+            <td>{item.teamName}</td>
+            <td>Captatin</td>
+            <td>Edit | <a href="#" onClick={()=>removeRow(a)}>Delete</a></td>
+          </tr>
+          ))}
+        </tbody>
+      </table>    
     </div>
   );
 }
 
-export default App;
+function App(){
+  return(
+    <div>
+      <PlayerData/>
+    </div>
+  )
+}
+
+export default App
